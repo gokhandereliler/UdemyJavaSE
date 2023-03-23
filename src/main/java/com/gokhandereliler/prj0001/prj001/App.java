35e3cd15e7577c70543492ed45197dc845707492
gokhandereliler <gokhandereliler@gmail.com>
2023-03-24 00:12:00 +0300

    first commit

---
+++ src/main/java/com/gokhandereliler/prj0001/prj001/App.java
@@ -0,0 +1,13 @@
+package com.gokhandereliler.prj0001.prj001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
